5396616712a1a527e795694b81f161f19e9cbc3f
B M <>
2023-05-04 16:41:26 +0300

    add name

---
+++ BuildingCompany/src/main/java/com/solvd/laba/BuildingCompany.java
@@ -0,0 +1,13 @@
+package com.solvd.laba;
+
+/**
+ * Hello world!
+ *
+ */
+public class BuildingCompany
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
